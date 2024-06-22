1000137479179bec28aab84a84b849e722bd72e6
Raj Patel <rp8637691@gmail.com>
2024-06-22 15:11:12 +0530

    upload files

---
+++ app.js
@@ -0,0 +1,103 @@
+let body=document.querySelector('body');
+body.classList.add("bodybackground")
+let gameSeq=[];
+let userSeq=[];
+
+let btns=["yellow","red","purple","green"];
+
+let started=false;
+let level=0;
+let HS=0;
+let h2=document.querySelector("h2");
+
+document.addEventListener("keypress",function()
+{
+    if(started==false)
+        {
+            started=true;
+            levelUp();
+        }
+});
+
+function gameFlash(btn)
+{
+    btn.classList.add("flash");
+    setTimeout(function()
+    {
+        btn.classList.remove("flash");
+    },250);
+}
+
+
+function userFlash(btn)
+{
+    btn.classList.add("userflash");
+    setTimeout(function()
+    {
+        btn.classList.remove("userflash");
+    },250);
+}
+
+function levelUp()
+{
+    userSeq=[];
+    level++;
+    h2.innerText=`Level ${level}`;
+    let randIdx=Math.floor(Math.random()*4);
+    let randColor=btns[randIdx];
+    let randBtn=document.querySelector(`.${randColor}`);
+    gameSeq.push(randColor);
+    gameFlash(randBtn);
+}
+
+function checkAns(idx)
+{
+    if(userSeq[idx] === gameSeq[idx]){
+        if(userSeq.length == gameSeq.length){
+            setTimeout(levelUp,1000);
+        }
+    }
+    else{
+        h2.innerHTML=`Game over! Your score was <b>${level}</b> <br> Press any key to start`;
+        body.classList.remove("bodybackground")
+        body.style.backgroundColor="red";
+        setTimeout(function()
+    {
+        body.classList.add("bodybackground");
+    },150);
+    highScore();
+    reset();
+    }
+}
+
+function btnPress(){
+    let btn=this;
+    userFlash(btn);
+    let userColor=btn.getAttribute("id");
+    userSeq.push(userColor);
+    checkAns(userSeq.length-1);
+}
+
+
+let allbtns=document.querySelectorAll(".btn");
+for(btn of allbtns)
+    {
+        btn.addEventListener("click",btnPress);
+    }
+   
+    function reset()
+    {
+        started=false;
+        gameSeq=[];
+        userSeq=[];
+        level=0;
+    }
+
+function highScore()
+    {
+        if (level >= HS) {
+            HS = level;
+            let h3 = document.querySelector("h3");
+            h3.innerText = `High Score: ${HS}`;
+        }
+    }
